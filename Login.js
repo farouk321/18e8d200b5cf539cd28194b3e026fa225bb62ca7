93f29f194991d5e9e3d2b022e4fb21f8f4255b9d
farouk321 <83145696+farouk321@users.noreply.github.com>
2023-07-05 13:24:14 +0100

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,13 +1,22 @@
 //-----------------Login-----------------//
 function Login(){
-	var http = new XMLHttpRequest();
-	var url = 'https://merch.amazon.com/manage/designs';
-	http.open('GET', url, false);
-	http.onreadystatechange = function() {
-		if (http.readyState == 4 && http.status == 200) {
-			User = http.responseText.match(/customerId":"([^"]+)"/)[1];
-			User2 = http.responseText.match(/accountId":"([^"]+)"/)[1];
-		}
-	};
-	http.send();
+	if (this.LOGIN) return new Promise.resolve(this.LOGIN);
+	var A={};
+	this.LOGIN=A;
+	return new Promise(r=>{
+		var http = new XMLHttpRequest();
+		var url = 'https://merch.amazon.com/manage/designs';
+		http.open('GET', url, false);
+		http.onreadystatechange = function() {
+			if (http.readyState == 4 && http.status == 200) {
+				let text=http.responseText
+				let U = text.match(/customerId":"([^"]+)"/)[1];
+				let U2 = text.match(/accountId":"([^"]+)"/)[1];
+				A.U=U;
+				A.U2=U2;
+				r(A)
+			}
+		};
+		http.send();
+	});
 }
