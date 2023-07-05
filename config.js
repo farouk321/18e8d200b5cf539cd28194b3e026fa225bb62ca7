de77a1b2cbe3725d0e0d599cb8decb5485bd28b3
farouk321 <83145696+farouk321@users.noreply.github.com>
2023-07-05 14:08:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,5 @@ require=({
 		"FindListing": {
 			deps: ["RXhrJSON","GetSales","Login","groupBy","Sleep"],
 		}
-	},
-	baseUrl: "https://cdn.jsdelivr.net/gh/farouk321/18e8d200b5cf539cd28194b3e026fa225bb62ca7@main/"
+	}
 });
