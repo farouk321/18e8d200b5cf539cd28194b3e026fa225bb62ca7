c34d17a3f0e3f8be9cc866a85f0d73694bbd9c02
farouk321 <83145696+farouk321@users.noreply.github.com>
2023-12-27 16:16:38 +0100

    Update LoadPrice.js

--- LoadPrice.js
+++ LoadPrice.js
@@ -4,71 +4,72 @@ async function LoadPrice(){
 	await GetS(365);
 	return (this.LoadPriceU)||(this.LoadPriceU=new LoadN());
 	function LoadN(){
-		var Price={};
-
-		var PriceDiv = {
-			 PHONE_CASE_APPLE_IPHONE: [1.436, 0, 0, 0, 0, 0, 1.4], 
-			 PHONE_CASE_SAMSUNG_GALAXY: [1.436, 0, 0, 0, 0, 0, 1.4], 
-			 RAGLAN: [1.362, 1.501, 1.605, 1.575, 1.599, 1.639, 0], 
-			 ZIP_HOODIE: [1.407, 1.633, 1.714, 1.577, 1.616, 1.623, 1.344], 
-			 STANDARD_SWEATSHIRT: [1.338, 1.587, 1.659, 1.547, 1.58, 1.584, 1.823], 
-			 TOTE_BAG: [1.206, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_TSHIRT: [1.351, 1.514, 1.584, 1.526, 1.553, 1.554, 1.35], 
-			 STANDARD_LONG_SLEEVE: [1.393, 1.58, 1.662, 1.523, 1.542, 1.602, 1.603], 
-			 VNECK: [1.337, 1.538, 1.607, 1.549, 1.577, 1.587, 0], 
-			 THROW_PILLOW: [1.24, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_PULLOVER_HOODIE: [1.386, 1.562, 1.653, 1.516, 1.54, 1.553, 1.755], 
-			 PREMIUM_TSHIRT: [1.323, 0, 0, 0, 0, 0, 0], 
-			 POP_SOCKET: [7.142, 8.571, 8.5, 8.571, 8.714, 8.642, 0], 
-			 TANK_TOP: [1.311, 1.584, 1.654, 1.482, 1.51, 1.547, 0]
-		},
-		PriceZero = {
-			 PHONE_CASE_APPLE_IPHONE: [13.83, 0, 0, 0, 0, 0, 1617.04], 
-			 PHONE_CASE_SAMSUNG_GALAXY: [13.83, 0, 0, 0, 0, 0, 1617.04], 
-			 RAGLAN: [18.76, 14.62, 15.96, 16.86, 16.88, 16.74, 0], 
-			 ZIP_HOODIE: [26.82, 23.1, 25.87, 25.29, 25.63, 25.17, 3757], 
-			 STANDARD_SWEATSHIRT: [25.15, 24.04, 26.35, 22.97, 23.35, 23.3, 2975.53], 
-			 TOTE_BAG: [15.54, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_TSHIRT: [13.38, 12.09, 13.72, 13.75, 13.75, 13.32, 1616.22], 
-			 STANDARD_LONG_SLEEVE: [17.67, 16.51, 17.82, 16.19, 16.2, 16.01, 2148.14], 
-			 VNECK: [15.16, 12.9, 13.54, 14.53, 14.5, 14.05, 0], 
-			 THROW_PILLOW: [17.38, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_PULLOVER_HOODIE: [25.03, 25.6, 28.63, 25.54, 25.9, 25.43, 3345.86], 
-			 PREMIUM_TSHIRT: [15.06, 0, 0, 0, 0, 0, 0], 
-			 POP_SOCKET: [0, 0.02, -0.01, 0.02, -0.02, 0.03, 0], 
-			 TANK_TOP: [15.27, 13.38, 14.25, 13.86, 13.86, 13.7, 0]
-		},
-		PriceMax = {
-			 PHONE_CASE_APPLE_IPHONE: [30, 0, 0, 0, 0, 0, 6500], 
-			 PHONE_CASE_SAMSUNG_GALAXY: [30, 0, 0, 0, 0, 0, 6500], 
-			 RAGLAN: [45, 30, 35, 35, 35, 35, 0], 
-			 ZIP_HOODIE: [60, 45, 50, 50, 50, 50, 8000], 
-			 STANDARD_SWEATSHIRT: [60, 45, 50, 50, 50, 50, 8000], 
-			 TOTE_BAG: [25, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_TSHIRT: [35, 30, 30, 30, 30, 30, 6500], 
-			 STANDARD_LONG_SLEEVE: [40, 35, 35, 40, 40, 40, 6500], 
-			 VNECK: [35, 35, 35, 35, 35, 35, 0], 
-			 THROW_PILLOW: [45, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_PULLOVER_HOODIE: [60, 45, 50, 50, 50, 50, 8000], 
-			 PREMIUM_TSHIRT: [45, 0, 0, 0, 0, 0, 0], 
-			 POP_SOCKET: [20, 20, 20, 20, 20, 20, 0], 
-			 TANK_TOP: [35, 30, 30, 30, 30, 30, 0]
-		},
-		PriceMin = {
-			 PHONE_CASE_APPLE_IPHONE: [0, 0, 0, 0, 0, 0, 0], 
-			 PHONE_CASE_SAMSUNG_GALAXY: [0, 0, 0, 0, 0, 0, 0], 
-			 RAGLAN: [0, 0, 0, 0, 0, 0, 0], 
-			 ZIP_HOODIE: [0, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_SWEATSHIRT: [0, 0, 0, 0, 0, 0, 0], 
-			 TOTE_BAG: [0, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_TSHIRT: [0, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_LONG_SLEEVE: [0, 0, 0, 0, 0, 0, 0], 
-			 VNECK: [0, 0, 0, 0, 0, 0, 0], 
-			 THROW_PILLOW: [0, 0, 0, 0, 0, 0, 0], 
-			 STANDARD_PULLOVER_HOODIE: [0, 0, 0, 0, 0, 0, 0], 
-			 PREMIUM_TSHIRT: [0, 0, 0, 0, 0, 0, 0], 
-			 POP_SOCKET: [11.72, 9.68, 10.34, 12.29, 12.28, 11.86, 0], 
-			 TANK_TOP: [0, 0, 0, 0, 0, 0, 0]
+		var Price={},
+		PriceObj = {
+			"Div": {
+				 "PHONE_CASE_APPLE_IPHONE": [1.436, 0, 0, 0, 0, 0, 1.4], 
+				 "PHONE_CASE_SAMSUNG_GALAXY": [1.436, 0, 0, 0, 0, 0, 1.4], 
+				 "RAGLAN": [1.362, 1.501, 1.605, 1.575, 1.599, 1.639, 0], 
+				 "ZIP_HOODIE": [1.407, 1.633, 1.714, 1.577, 1.616, 1.623, 1.344], 
+				 "STANDARD_SWEATSHIRT": [1.338, 1.587, 1.659, 1.547, 1.58, 1.584, 1.823], 
+				 "TOTE_BAG": [1.206, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_TSHIRT": [1.351, 1.514, 1.584, 1.526, 1.553, 1.554, 1.35], 
+				 "STANDARD_LONG_SLEEVE": [1.393, 1.58, 1.662, 1.523, 1.542, 1.602, 1.603], 
+				 "VNECK": [1.337, 1.538, 1.607, 1.549, 1.577, 1.587, 0], 
+				 "THROW_PILLOW": [1.24, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_PULLOVER_HOODIE": [1.386, 1.562, 1.653, 1.516, 1.54, 1.553, 1.755], 
+				 "PREMIUM_TSHIRT": [1.323, 0, 0, 0, 0, 0, 0], 
+				 "POP_SOCKET": [7.142, 8.571, 8.5, 8.571, 8.714, 8.642, 0], 
+				 "TANK_TOP": [1.311, 1.584, 1.654, 1.482, 1.51, 1.547, 0]
+			},
+			"Zero": {
+				 "PHONE_CASE_APPLE_IPHONE": [13.83, 0, 0, 0, 0, 0, 1617.04], 
+				 "PHONE_CASE_SAMSUNG_GALAXY": [13.83, 0, 0, 0, 0, 0, 1617.04], 
+				 "RAGLAN": [18.76, 14.62, 15.96, 16.86, 16.88, 16.74, 0], 
+				 "ZIP_HOODIE": [26.79, 23.1, 25.87, 25.29, 25.63, 25.17, 3757], 
+				 "STANDARD_SWEATSHIRT": [25.12, 24.04, 26.35, 22.97, 23.35, 23.3, 2975.53], 
+				 "TOTE_BAG": [15.54, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_TSHIRT": [13.38, 12.06, 13.72, 13.71, 13.72, 13.28, 1616.22], 
+				 "STANDARD_LONG_SLEEVE": [17.67, 16.51, 17.82, 16.19, 16.2, 16.01, 2148.14], 
+				 "VNECK": [15.16, 12.9, 13.54, 14.53, 14.5, 14.05, 0], 
+				 "THROW_PILLOW": [17.38, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_PULLOVER_HOODIE": [25.04, 25.61, 28.61, 25.56, 25.92, 25.39, 3345.86], 
+				 "PREMIUM_TSHIRT": [15.06, 0, 0, 0, 0, 0, 0], 
+				 "POP_SOCKET": [0, 0.02, -0.01, 0.02, -0.02, 0.03, 0], 
+				 "TANK_TOP": [15.27, 13.38, 14.25, 13.86, 13.86, 13.7, 0]
+			},
+			"Max": {
+				 "PHONE_CASE_APPLE_IPHONE": [30, 0, 0, 0, 0, 0, 6500], 
+				 "PHONE_CASE_SAMSUNG_GALAXY": [30, 0, 0, 0, 0, 0, 6500], 
+				 "RAGLAN": [45, 30, 35, 35, 35, 35, 0], 
+				 "ZIP_HOODIE": [37.99, 45, 50, 50, 50, 50, 8000], 
+				 "STANDARD_SWEATSHIRT": [33.99, 45, 50, 50, 50, 50, 8000], 
+				 "TOTE_BAG": [25, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_TSHIRT": [35, 17.48, 19.98, 19.48, 19.48, 19.48, 6500], 
+				 "STANDARD_LONG_SLEEVE": [40, 35, 35, 40, 40, 40, 6500], 
+				 "VNECK": [35, 35, 35, 35, 35, 35, 0], 
+				 "THROW_PILLOW": [45, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_PULLOVER_HOODIE": [35.99, 33.99, 38.99, 33.99, 33.99, 33.99, 8000], 
+				 "PREMIUM_TSHIRT": [45, 0, 0, 0, 0, 0, 0], 
+				 "POP_SOCKET": [20, 20, 20, 20, 20, 20, 0], 
+				 "TANK_TOP": [35, 30, 30, 30, 30, 30, 0]
+			},
+			"Min": {
+				 "PHONE_CASE_APPLE_IPHONE": [0, 0, 0, 0, 0, 0, 0], 
+				 "PHONE_CASE_SAMSUNG_GALAXY": [0, 0, 0, 0, 0, 0, 0], 
+				 "RAGLAN": [0, 0, 0, 0, 0, 0, 0], 
+				 "ZIP_HOODIE": [26.99, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_SWEATSHIRT": [24.99, 0, 0, 0, 0, 0, 0], 
+				 "TOTE_BAG": [0, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_TSHIRT": [0, 12.07, 13.71, 13.75, 13.75, 13.31, 0], 
+				 "STANDARD_LONG_SLEEVE": [0, 0, 0, 0, 0, 0, 0], 
+				 "VNECK": [0, 0, 0, 0, 0, 0, 0], 
+				 "THROW_PILLOW": [0, 0, 0, 0, 0, 0, 0], 
+				 "STANDARD_PULLOVER_HOODIE": [24.99, 25.62, 28.65, 25.52, 25.92, 25.42, 0], 
+				 "PREMIUM_TSHIRT": [0, 0, 0, 0, 0, 0, 0], 
+				 "POP_SOCKET": [11.72, 9.68, 10.34, 12.29, 12.28, 11.86, 0], 
+				 "TANK_TOP": [0, 0, 0, 0, 0, 0, 0]
+			}
 		},des=.99,desJP=00,
 		marplace={"US":["USD",0,1],"GB":["GBP",.2,1],"DE":["EUR",.19,1],"FR":["EUR",.2,1],"IT":["EUR",.22,1],"ES":["EUR",.21,1],"JP":["JPY", .1,200]};
 		marplace.T=Object.keys(marplace);
@@ -106,10 +107,10 @@ async function LoadPrice(){
 			var r;
 			let M=marplace.T[b];
 			let Mk=marplace[M];
-			let div=PriceDiv[a][b];
-			let zero=PriceZero[a][b];
-			let max=PriceMax[a][b];
-			let min=PriceMin[a][b];
+			let div=PriceObj.Div[a][b];
+			let zero=PriceObj.Zero[a][b];
+			let max=PriceObj.Max[a][b];
+			let min=PriceObj.Min[a][b];
 			if (!div) return 0;
 			r=(1+Mk[1])*(zero+Mk[2]*p*div);
 			return Prsi(r);
@@ -134,19 +135,19 @@ async function LoadPrice(){
 			function Prc(u){
 				var JC=0,Id,G=0,FT=0,To=0;
 				if (!marplace.T) marplace.T=Object.keys(marplace);
-				if (!marplace.P) marplace.P=Object.keys(PriceZero);
+				if (!marplace.P) marplace.P=Object.keys(PriceObj.Zero);
 				//--------------------------------------------
-				Price[u]=JSON.parse(JSON.stringify(PriceZero));
+				Price[u]=JSON.parse(JSON.stringify(PriceObj.Zero));
 				for (var JC=0;JC<marplace.P.length;JC++){
 					Id=marplace.P[JC];
-					To+=PriceZero[Id].length;
+					To+=PriceObj.Zero[Id].length;
 					if (JC==marplace.P.length-1) FT=1;
-					for (var i=0;i<PriceZero[Id].length;i++){
+					for (var i=0;i<PriceObj.Zero[Id].length;i++){
 						RY(Id,i,u);
 					}
 				}
 				function RY(a,b,u){
-					if (!Price.i) PriceZero[a][b]=R(PriceZero[a][b]/(1+marplace[marplace.T[b]][1]),100);
+					if (!Price.i) PriceObj.Zero[a][b]=R(PriceObj.Zero[a][b]/(1+marplace[marplace.T[b]][1]),100);
 					Price[u][a][b]=Ry(a,b,u);
 					G+=1;
 					if (G==To&&FT) console.log("Price:"+u+", "+G+"/"+To);
