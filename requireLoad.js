f3f82799f0449cdb7fe9f423782a0ccb4e3fa40a
farouk321 <83145696+farouk321@users.noreply.github.com>
2023-12-30 16:19:27 +0100

    Create requireLoad.js

---
+++ requireLoad.js
@@ -0,0 +1,33 @@
+    //-----------------requireLoad----------------------
+    async function Load(nd,fn=((e)=>e)){
+        await requireLoad()
+        Login()
+        return prequirejs([,...nd]).then(fn);
+        function prequirejs(ar){
+            return new Promise((r,f)=>requirejs(ar,r))
+        }
+        async function requireLoad() {
+            let v='latest';
+            if (this.requirejs) return Promise.resolve(0);
+            var branch='https://cdn.jsdelivr.net/gh/farouk321/18e8d200b5cf539cd28194b3e026fa225bb62ca7@'+v;
+            require = {baseUrl:branch,deps: ["config"]};
+            await lS(branch+"/require.js")
+            return new Promise((r,f)=>setTimeout(Tim=(e=>{try{requirejs.LoadPromise.then(r)}catch(e){setTimeout(Tim,100)}})),100);
+            function lS(src,async=true,type="text/javascript"){
+                return new Promise((resolve,reject)=>{
+                    try{
+                        const tag=document.createElement("script");
+                        const container=document.head||document.body;
+                        tag.type=type;
+                        tag.async=async;
+                        tag.src=src;
+                        tag.addEventListener("load",()=>{resolve(1)});
+                        tag.addEventListener("error",()=>{reject(0)});
+                        container.appendChild(tag);
+                    }catch(error){
+                        reject(error);
+                    }
+                })
+            };
+        }
+    }
