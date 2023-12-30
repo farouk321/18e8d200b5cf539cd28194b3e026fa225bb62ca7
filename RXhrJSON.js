ab653a260581b6cffd44a420a0750dc72421884a
farouk321 <83145696+farouk321@users.noreply.github.com>
2023-12-30 14:27:05 +0100

    Update RXhrJSON.js

--- RXhrJSON.js
+++ RXhrJSON.js
@@ -1,29 +1,29 @@
 //-----------------RXhrJSON-----------------//
 function RXhrJSONP(method, url, post, Rc = {count:6,timeout:1e4}) {
-return new Promise((call, fail) => { RXhrJSON(method, url, post, call, Rc, fail) });
+	return new Promise((call, fail) => { RXhrJSON(method, url, post, call, Rc, fail) });
 }
 function RXhrJSON(method, url,post,call,Rc={count:6,timeout:1e4},fail=()=>{}){
-    (function R(s=0){
-	if (Rc.count==0) return fail();
-	await sleep(s);
-	Rc.count--;
-	let http = new XMLHttpRequest();
-	http.open(method, url, true);
-	http.setRequestHeader('Content-Type', 'text/plain');
-	http.onreadystatechange = async function() {
-	    if (http.readyState == 4) {
-		if (http.status == 200){
-		    try{
-			var O = JSON.parse(http.responseText);
-		    }catch(e){
-			return R(1000);
+    (async function R(s=0){
+		if (Rc.count==0) return fail();
+		await sleep(s);
+		Rc.count--;
+		let http = new XMLHttpRequest();
+		http.open(method, url, true);
+		http.setRequestHeader('Content-Type', 'text/plain');
+		http.onreadystatechange = async function() {
+		    if (http.readyState == 4) {
+				if (http.status == 200){
+				    try{
+						var O = JSON.parse(http.responseText);
+				    }catch(e){
+						return R(1000);
+				    }
+				    call(O);
+				}else{
+				    return R(Rc.timeout);
+				}
 		    }
-		    call(O);
-		}else{
-		    return R(Rc.timeout);
 		}
-	    }
-	}
-	http.send(post);
+		http.send(typeof post == "string"?post:JSON.stringify(post));
     })();
 }
