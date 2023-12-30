dab1416ffbd35ba44c87f3a24f7fa2180024517e
farouk321 <83145696+farouk321@users.noreply.github.com>
2023-12-30 15:46:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -33,4 +33,4 @@ requirejs.config({
 		},
 	}
 });
-await (new Promise((r,f)=>requirejs(["RXhrJSON","Login","groupBy","Sleep"],r)))
+requirejs.LoadPromise=new Promise((r,f)=>requirejs(["RXhrJSON","Login","groupBy","Sleep"],r))
