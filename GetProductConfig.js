5870a855eda4803ddc6793533e0002bf7fa37481
farouk321 <83145696+farouk321@users.noreply.github.com>
2024-12-08 18:48:54 +0100

    Update GetProductConfig.js

--- GetProductConfig.js
+++ GetProductConfig.js
@@ -1,10 +1,11 @@
 //-----------------GetProductConfig-----------------//
 async function GetProductConfig() {
+  let L = await Login();
   var url = 'https://merch.amazon.com/api/ng-amazon/coral/com.amazon.merch.sellerdesignservice.MerchSellerDesignService/GetProductTypeConfiguration';
   var post = {
-    "actingAsId": User,
-    "userId": User,
-    "accountId": User2,
+    "actingAsId": L.U,
+    "userId": L.U,
+    "accountId": L.U2,
     "__type": "com.amazon.merch.sellerdesignservice#GetProductTypeConfigurationInput",
   };
   return RXhrJSONP("POST",url,post,{count:2,timeout:3e3}).then(e=>e.productTypeConfiguration)
